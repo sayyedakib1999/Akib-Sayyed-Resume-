30f1523c398f35893ad3fcc9f9b31d17fcad4ee8
akib <sayyedakib104@gmail.com>
2024-03-02 20:44:00 +0530

    Updated

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// <!-- Hover 0 Start -->
 document.addEventListener('DOMContentLoaded', function() {
     const text = document.getElementById('text');
     const image = document.getElementById('image');
@@ -10,4 +11,64 @@ document.addEventListener('DOMContentLoaded', function() {
       image.style.display = 'none';
     });
   });
+// Hover 0 End
+
+//  Hover 1 Start 
+  document.addEventListener('DOMContentLoaded', function() {
+    const text = document.getElementById('text1');
+    const image = document.getElementById('image1');
   
+    text.addEventListener('mouseover', function() {
+      image.style.display = 'block';
+    });
+  
+    text.addEventListener('mouseout', function() {
+      image.style.display = 'none';
+    });
+  });
+// Hover 1 end 
+
+//  Hover 2 Start
+  document.addEventListener('DOMContentLoaded', function() {
+    const text = document.getElementById('text2');
+    const image = document.getElementById('image2');
+  
+    text.addEventListener('mouseover', function() {
+      image.style.display = 'block';
+    });
+  
+    text.addEventListener('mouseout', function() {
+      image.style.display = 'none';
+    });
+  });
+  //  Hover 2 end
+
+  //  Hover 3 Start
+  document.addEventListener('DOMContentLoaded', function() {
+    const text = document.getElementById('text3');
+    const image = document.getElementById('image3');
+  
+    text.addEventListener('mouseover', function() {
+      image.style.display = 'block';
+    });
+  
+    text.addEventListener('mouseout', function() {
+      image.style.display = 'none';
+    });
+  });
+  //  Hover 3 end
+
+  //  Hover 4 Start
+  document.addEventListener('DOMContentLoaded', function() {
+    const text = document.getElementById('text4');
+    const image = document.getElementById('image4');
+  
+    text.addEventListener('mouseover', function() {
+      image.style.display = 'block';
+    });
+  
+    text.addEventListener('mouseout', function() {
+      image.style.display = 'none';
+    });
+  });
+  //  Hover 4end
